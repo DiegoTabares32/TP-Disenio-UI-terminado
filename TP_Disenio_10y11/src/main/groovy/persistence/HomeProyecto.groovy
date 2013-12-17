6ecf80d2b466a1cce6cdb11e0e0254d081860b79
jaecisneros <jaecisneros@gmail.com>
2013-12-17 19:54:49 -0300

    Saque el codigo repetido y extraje las condiciones a funciones de orden superior.

--- TP_Disenio_10y11/src/main/groovy/persistence/HomeProyecto.groovy
+++ TP_Disenio_10y11/src/main/groovy/persistence/HomeProyecto.groovy
@@ -17,13 +17,13 @@ public class HomeProyecto {
 		return getManager().createQuery("from Proyecto").getResultList();
 	}
 
-	def getTareasMenoresADiezDias(Proyecto proyecto){
+	def get(Proyecto proyecto, condicion){
 		def resultados = []
 		EntityManager session = getManager()
 		session.open
 		try {
 			resultados = proyecto.tareas.each{ t ->
-				if (t.getTiempoTotal() < 10){
+				if (condicion(t)){
 					resultados.add(t)
 				}
 			}
@@ -33,20 +33,14 @@ public class HomeProyecto {
 		resultados
 	}
 
-	def getTareasConMasDeDosSubtareas(Proyecto proyecto){
-		def resultados = []
-		EntityManager session = getManager()
-		session.open
-		try {
-			resultados = proyecto.tareas.each{ t ->
-				if (t.getCantidadSubtareas() > 2){
-					resultados.add(t)
-				}
-			}
-		} finally {
-			session.close()
-		}
-		resultados
+	def getTareasMenoresADiezDias(proyecto){
+		this.get(proyecto, {t -> t.getTiempoTotal() < 10})
+		 
+	}
+	
+	def getTareasConMasDeDosSubtareas(proyecto){
+		this.get(proyecto, {t -> t.getCantidadSubtareas() > 2})
+		 
 	}
 	
 }
